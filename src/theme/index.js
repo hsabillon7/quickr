c7a953319b43754129e83b88282ba7d757c78a6c
Héctor Sabillón <h.hsabillon@gmail.com>
2021-03-01 21:16:02 -0600

    Estilos personalizados

---
+++ src/theme/index.js
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    primary: "#3369ff",
+    secondary: "#ffda47",
+    backgroundDark: "#171c26",
+    purple: "#ae3b76",
+    green: "#0aebaf",
+    orange: "#ff7746",
+  },
+};
+
+export default theme;
