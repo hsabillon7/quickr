3b088c0d364aa0a4fe0da1dd30cf54d0bc4649ac
Héctor Sabillón <h.hsabillon@gmail.com>
2021-03-02 21:03:50 -0600

    Implementación de validaciones en el formulario de registro

--- src/components/forms/SignupForm.js
+++ src/components/forms/SignupForm.js
@@ -3,19 +3,48 @@ import { StyleSheet, View } from "react-native";
 import { Input, Button } from "react-native-elements";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { firebase } from "../../firebase";
+import { validate } from "email-validator";
 
-const SignupForm = () => {
+const SignupForm = ({ navigation }) => {
   const [fullname, setFullname] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [fullnameError, setFullnameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [confirmPasswordError, setConfirmPasswordError] = useState(false);
+
+  // Verifica que los datos ingresados sean correctos
+  const handleVerify = (input) => {
+    if (input === "fullname") {
+      // Verificar el nombre del usuario
+      if (!fullname) setFullnameError(true);
+      else setFullnameError(false);
+    } else if (input === "email") {
+      // Verificar el correo electrónico
+      if (!email) setEmailError(true);
+      else if (!validate(email)) setEmailError(true);
+      else setEmailError(false);
+    } else if (input === "password") {
+      // Verificar la contraseña
+      if (!password) setPasswordError(true);
+      else if (password.length < 6) setPasswordError(true);
+      else setPasswordError(false);
+    } else if (input === "confirmPassword") {
+      // Verificar la confirmación de la contraseña
+      if (!confirmPassword) setConfirmPasswordError(true);
+      else if (confirmPassword !== password) setConfirmPasswordError(true);
+      else setConfirmPasswordError(false);
+    }
+  };
 
   const handleSignup = () => {
     firebase
       .auth()
       .createUserWithEmailAndPassword(email, password)
       .then((response) => {
-        console.log(response);
+        navigation.navigate("Signin", { userCreated: true });
       })
       .catch((error) => console.log(error));
   };
@@ -27,24 +56,57 @@ const SignupForm = () => {
         leftIcon={<Icon name="user" />}
         value={fullname}
         onChangeText={setFullname}
+        onBlur={() => {
+          handleVerify("fullname");
+        }}
+        errorMessage={
+          fullnameError ? "Por favor ingresa tu nombre completo" : ""
+        }
       />
       <Input
         placeholder="Email"
         leftIcon={<Icon name="envelope" />}
         value={email}
         onChangeText={setEmail}
+        autoCapitalize="none"
+        onBlur={() => {
+          handleVerify("email");
+        }}
+        errorMessage={
+          emailError ? "Por favor ingresa una dirección de correo válida" : ""
+        }
       />
       <Input
         placeholder="Password"
         leftIcon={<Icon name="lock" />}
         value={password}
         onChangeText={setPassword}
+        secureTextEntry
+        autoCapitalize="none"
+        onBlur={() => {
+          handleVerify("password");
+        }}
+        errorMessage={
+          passwordError
+            ? "Por favor ingresa una contraseña de mínimo 6 caracteres"
+            : ""
+        }
       />
       <Input
         placeholder="Confirm password"
         leftIcon={<Icon name="lock" />}
         value={confirmPassword}
         onChangeText={setConfirmPassword}
+        secureTextEntry
+        autoCapitalize="none"
+        onBlur={() => {
+          handleVerify("confirmPassword");
+        }}
+        errorMessage={
+          confirmPasswordError
+            ? "Por favor reingresa la contraseña y verifica que es correcta"
+            : ""
+        }
       />
       <Button title="Create account" onPress={handleSignup} />
     </View>
