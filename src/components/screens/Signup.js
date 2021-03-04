18f2e435847815d6d9a72070ca6959e03ba84f36
Héctor Sabillón <h.hsabillon@gmail.com>
2021-03-04 09:50:18 -0600

    Mejora de estilos

--- src/components/screens/Signup.js
+++ src/components/screens/Signup.js
@@ -3,12 +3,13 @@ import { Dimensions, StyleSheet, TouchableOpacity, View } from "react-native";
 import { Button, Input, Text } from "react-native-elements";
 import Logo from "../shared/Logo";
 import SignupForm from "../forms/SignupForm";
+import theme from "../../theme";
 
 const { width, height } = Dimensions.get("screen");
 
 const Signup = ({ navigation }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Logo />
       <SignupForm />
       <TouchableOpacity
@@ -22,6 +23,13 @@ const Signup = ({ navigation }) => {
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 10,
+    backgroundColor: theme.colors.backgroundWhite,
+  },
+});
 
 export default Signup;
