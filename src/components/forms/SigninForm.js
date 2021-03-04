cdeb670c2221ba783867a6eb1c7c7217489f249e
Héctor Sabillón <h.hsabillon@gmail.com>
2021-03-04 09:51:39 -0600

    Implementación de la validación del formulario de inicio de sesión. También se incluye el código necesario para verificar si se logra una autenticación con firebase

--- src/components/forms/SigninForm.js
+++ src/components/forms/SigninForm.js
@@ -2,27 +2,68 @@ import React, { useState } from "react";
 import { StyleSheet, View, Text } from "react-native";
 import { Input, Button } from "react-native-elements";
 import Icon from "react-native-vector-icons/FontAwesome";
+import { validate } from "email-validator";
+import { firebase } from "../../firebase";
+import Alert from "../shared/Alert";
 
 const SigninForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [error, setError] = useState("");
+
+  // Verifica que se ingresan los datos del email y el password
+  const handleVerify = (input) => {
+    if (input === "email") {
+      if (!email) setEmailError(true);
+      else if (!validate(email)) setEmailError(true);
+      else setEmailError(false);
+    } else if (input === "password") {
+      if (!password) setPasswordError(true);
+      else setPasswordError(false);
+    }
+  };
+
+  const handleSignin = () => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((response) => console.log(response))
+      .catch((error) => {
+        console.log(error);
+        setError(error.message);
+      });
+  };
 
   return (
     <View>
+      {error ? <Alert title={error} type="error" /> : null}
       <Input
         placeholder="Email"
         leftIcon={<Icon name="envelope" />}
         value={email}
-        onChange={setEmail}
+        onChangeText={setEmail}
+        onBlur={() => {
+          handleVerify("email");
+        }}
+        errorMessage={
+          emailError
+            ? "Por favor ingresa tu cuenta de correo electrónico"
+            : null
+        }
       />
       <Input
         placeholder="Password"
         leftIcon={<Icon name="lock" />}
         value={password}
-        onChange={setPassword}
+        onChangeText={setPassword}
+        onBlur={() => {
+          handleVerify("password");
+        }}
+        errorMessage={passwordError ? "Por favor ingresa tu contraseña" : null}
       />
-      <Button title="Signin" />
+      <Button title="Signin" onPress={handleSignin} />
     </View>
   );
 };
