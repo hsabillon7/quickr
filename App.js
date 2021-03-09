536bc01ed2c261a27b36d1b9a3cb28cc5ae2d647
Héctor Sabillón <h.hsabillon@gmail.com>
2021-03-08 21:46:56 -0600

    Se implementa la función de la verificación de credenciales de autenticación y se muestra un stack diferente para usuario autenticados y no autenticados

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { ThemeProvider } from "react-native-elements";
@@ -9,23 +9,42 @@ import Signin from "./src/components/screens/Signin";
 import Signup from "./src/components/screens/Signup";
 import Home from "./src/components/screens/Home";
 import theme from "./src/theme";
+import PersistLogin from "./src/utils/persistLogin";
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [user, setUser] = useState({});
+
+  // Verificar si ya existen credenciales de autenticación
+  useEffect(() => {
+    const userData = PersistLogin();
+    setUser(userData);
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaProvider>
         <NavigationContainer>
           <Stack.Navigator>
-            <Stack.Screen
-              name="Signin"
-              component={Signin}
-              initialParams={{ userCreated: false }}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen name="Signup" component={Signup} />
-            <Stack.Screen name="Home" component={Home} />
+            {/* Mostrar un Stack distinto dependiendo el nivel de autenticación */}
+            {user ? (
+              <Stack.Screen
+                name="Home"
+                component={Home}
+                initialParams={{ user: user }}
+              />
+            ) : (
+              <>
+                <Stack.Screen
+                  name="Signin"
+                  component={Signin}
+                  initialParams={{ userCreated: false }}
+                  options={{ headerShown: false }}
+                />
+                <Stack.Screen name="Signup" component={Signup} />
+              </>
+            )}
           </Stack.Navigator>
         </NavigationContainer>
       </SafeAreaProvider>
