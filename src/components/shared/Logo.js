4f472c39889d72aa1389ec78a6191d66b1c06d04
Héctor Sabillón <h.hsabillon@gmail.com>
2021-03-15 18:53:44 -0600

    Solución a un error en Android al momento de cargar la imagen

--- src/components/shared/Logo.js
+++ src/components/shared/Logo.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { Dimensions, StyleSheet, View } from "react-native";
 import { Image } from "react-native-elements";
+import QuickrLogo from "../../../assets/quickr_logo.png";
 
 const { width, height } = Dimensions.get("screen");
 
 const Logo = () => {
   return (
     <View style={styles.container}>
-      <Image
-        style={styles.logo}
-        source={{ uri: require("../../../assets/quickr_logo.png") }}
-      />
+      <Image style={styles.logo} source={QuickrLogo} />
     </View>
   );
 };
