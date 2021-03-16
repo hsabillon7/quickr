8c686bc251d114e2014f4288a62b1fac73d5d63d
Héctor Sabillón <h.hsabillon@gmail.com>
2021-03-15 19:56:42 -0600

    Implementación del flujo de autenticación para el usuario. Se muestra la pantalla de splash mientras la aplicación verifica la validez del token en caso de existir

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import * as SplashScreen from "expo-splash-screen";
 import { Context as AuthContext } from "../../providers/AuthContext";
 import Signin from "../screens/Signin";
 import Signup from "../screens/Signup";
@@ -16,27 +17,32 @@ const Navigation = () => {
     persistLogin();
   }, []);
 
-  useEffect(() => {
-    console.log(state.user);
-  }, [state.user]);
+  // Prevenir que se oculte la pantalla de splash
+  SplashScreen.preventAutoHideAsync();
+
+  // Ocultar la pantalla de splash al verificar que existe un token de inicio
+  if (!state.loading) SplashScreen.hideAsync();
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        {/* Mostrar un Stack distinto dependiendo el nivel de autenticación */}
-        {state.loggedIn ? (
-          <Stack.Screen name="Home" component={Home} />
-        ) : (
-          <>
-            <Stack.Screen
-              name="Signin"
-              component={Signin}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen name="Signup" component={Signup} />
-          </>
-        )}
-      </Stack.Navigator>
+      {!state.loading && (
+        <>
+          {state.loggedIn ? (
+            <Stack.Navigator>
+              <Stack.Screen name="Home" component={Home} />
+            </Stack.Navigator>
+          ) : (
+            <Stack.Navigator>
+              <Stack.Screen
+                name="Signin"
+                component={Signin}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen name="Signup" component={Signup} />
+            </Stack.Navigator>
+          )}
+        </>
+      )}
     </NavigationContainer>
   );
 };
