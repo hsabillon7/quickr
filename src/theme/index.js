fd6ba13b10514dab5ec07788194b296db0334960
Héctor Sabillón <h.hsabillon@gmail.com>
2021-03-24 08:48:44 -0600

    Corrección en la estructura de los colores del tema

--- src/theme/index.js
+++ src/theme/index.js
@@ -3,8 +3,9 @@ import { DefaultTheme } from "react-native-paper";
 const theme = {
   ...DefaultTheme,
   colors: {
+    ...DefaultTheme.colors,
     primary: "#3369ff",
-    secondary: "#ffda47",
+    accent: "#ffda47",
     backgroundDark: "#171c26",
     backgroundWhite: "#fff",
     purple: "#ae3b76",
